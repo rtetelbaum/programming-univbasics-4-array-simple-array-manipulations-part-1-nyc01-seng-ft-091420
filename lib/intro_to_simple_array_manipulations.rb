b9a5cf6b4d0a8197aaa8def59f6c3bf19b561ed5
Roman Tetelbaum <rtetelbaum@mailbox.org>
2020-08-24 23:33:30 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,7 +6,7 @@ def using_push(colors_in_the_rainbow, next_color)
   colors_in_the_rainbow.push(next_color)
 end
 
-using_push(colors_in_the_rainbow, next_color)
+puts using_push(colors_in_the_rainbow, next_color)
 
 
 bouroughs_in_nyc = ["Queens", "Brooklyn"]
@@ -17,7 +17,7 @@ def using_unshift(bouroughs_in_nyc, new_neighborhood)
   bouroughs_in_nyc.unshift(new_neighborhood)
 end
 
-using_unshift(bouroughs_in_nyc, new_neighborhood)
+puts using_unshift(bouroughs_in_nyc, new_neighborhood)
 
 
 continents = ["North America", "South America"]
@@ -26,7 +26,7 @@ def using_pop(continents)
   deleted_string = continents.pop
 end
 
-using_pop(continents)
+puts using_pop(continents)
 
 
 dog_breeds = ["Akita", "German Shephard", "Dalmation"]
@@ -35,7 +35,7 @@ def pop_with_args(dog_breeds)
   small_dogs = dog_breeds.pop(2)
 end
 
-pop_with_args(dog_breeds)
+puts pop_with_args(dog_breeds)
 
 
 my_favorite_cities = ["New York", "Los Angeles", "Chicago"]
@@ -44,7 +44,7 @@ def using_shift(my_favorite_cities)
   im_so_over_this_city = my_favorite_cities.shift
 end
 
-using_shift(my_favorite_cities)
+puts using_shift(my_favorite_cities)
 
 
 ice_cream_brands = ["Haagen Daz", "Ben & Jerry's", "Breyers"]
@@ -53,4 +53,4 @@ def shift_with_args(ice_cream_brands)
   brands_removed = ice_cream_brands.shift(2)
 end
 
-shift_with_args(ice_cream_brands)
+puts shift_with_args(ice_cream_brands)
