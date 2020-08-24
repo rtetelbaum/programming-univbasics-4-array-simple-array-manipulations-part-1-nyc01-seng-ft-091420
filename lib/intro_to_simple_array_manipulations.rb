f179e78a97b473dc848fb92d6f666aa6ef0c6d69
Roman Tetelbaum <rtetelbaum@mailbox.org>
2020-08-24 23:17:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -7,3 +7,13 @@ def using_push(colors_in_the_rainbow, next_color)
 end
 
 using_push(colors_in_the_rainbow, next_color)
+
+bouroughs_in_nyc = ["Queens", "Brooklyn"]
+
+new_neighborhood = "Manhattan"
+
+def using_unshift(bouroughs_in_nyc, new_neighborhood)
+  bouroughs_in_nyc.unshift(new_neighborhood)
+end
+
+using_unshift(bouroughs_in_nyc, new_neighborhood)
